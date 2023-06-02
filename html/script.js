0a2b5dbc90c8696c75767a052538f42c1b2f0a49
ding1226 <Alice24440961@gmail.com>
2023-06-02 17:27:47 +0800

    add script.js

--- html/script.js
+++ html/script.js
@@ -1,11 +1,28 @@
-document.addEventListener('keydown', jump);
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+var guessInput = document.getElementById('guess');
+var checkButton = document.getElementById('check');
+var message = document.getElementById('message');
 
-function jump(event) {
-  var dino = document.getElementById('dino');
-  if (event.keyCode === 32) {
-    dino.classList.add('jump-animation');
-    setTimeout(function() {
-      dino.classList.remove('jump-animation');
-    }, 500);
+checkButton.addEventListener('click', checkGuess);
+
+function checkGuess() {
+  var userGuess = parseInt(guessInput.value);
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+    message.textContent = '請輸入1到100之間的數字。';
+  } else if (userGuess === randomNumber) {
+    message.textContent = '恭喜你！猜對了！';
+    disableInputAndButton();
+  } else if (userGuess < randomNumber) {
+    message.textContent = '太低了，再試一次。';
+  } else {
+    message.textContent = '太高了，再試一次。';
   }
+
+  guessInput.value = '';
+  guessInput.focus();
+}
+
+function disableInputAndButton() {
+  guessInput.disabled = true;
+  checkButton.disabled = true;
 }
